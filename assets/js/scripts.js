c233aa8f2428818edfc7e3ec692fffa40b2f1ab8
mike121986 <maft_1986@hotmail.com>
2021-05-20 17:58:19 -0500

    ampliamo los datos para ver la url sea mas grande

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -132,7 +132,7 @@ w3Unpocodet0d0 */
 function getAbsolutePath() {
     var loc = window.location;
     //var pathName = loc.pathname.substring(0, loc.pathname.lastIndexOf('/') + 1);
-    var pathName = loc.pathname.substring(0, 6);
+    var pathName = loc.pathname.substring(0,8);
     // var pathName = loc.pathname.substring(0, 1);
     //console.log(pathName);
     return loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathName.length));    
