bfebc25e2f0171d28e72b4f61176f3f287ebed82
mike121986 <maft_1986@hotmail.com>
2021-05-20 18:03:33 -0500

    agregamos id para poder hacer ekl submit dspues de haber visto los datos en jquery

--- views/usuario/registro.php
+++ views/usuario/registro.php
@@ -7,25 +7,30 @@
 <?php endif; ?>
 <?php Utils::deleteSession('register'); ?>
 
-<form action="<?=base_url?>usuario/save" method="POST" novalidate class="container">
-	<label for="nombre">Nombre</label>
-	<input type="text" name="nombre" class="form-control"  id="nombre" />
-	
-	<label for="apellidos">Apellidos</label>
-	<input type="text" name="apellidos" class="form-control"  id="apellidos" />
-	
-	<label for="email">Email</label>
-	<input type="text" name="email" class="form-control"  id="emailRegistro" />
-	
-	<label for="password">Contraseña</label>
-	<div class="input-group input-group-sm ">
-		<input type="password" name="password" class="form-control"  id="passwordRegistro" aria-label="Small" aria-describedby="inputGroup-sizing-sm"/>
-		<div class="input-group-prepend">
-			<span class="input-group-text" id="inputGroup-sizing-sm"><i class="fa fa-eye eyeHiddeNo" aria-hidden="true"></i></span>
-		</div>
+<form action="<?=base_url?>usuario/save" method="POST" novalidate class="container" id="Frm-registro">
+	<div class="form-group">
+		<label for="nombre">Nombre</label>
+		<input type="text" name="nombre" class="form-control"  id="nombre" />
 	</div>
+	<div class="form-group">
+		<label for="apellidos">Apellidos</label>
+		<input type="text" name="apellidos" class="form-control"  id="apellidos" />
+	</div>
+	<div class="form-group">
+		<label for="email">Email</label>
+		<input type="text" name="email" class="form-control"  id="emailRegistro" />
+		<div class='spinnerWhite'></div>
+	</div>
+	<div class="form-group">
+		<label for="password">Contraseña</label>
+		<div class="input-group input-group-sm ">
+			<input type="password" name="password" class="form-control"  id="passwordRegistro" aria-label="Small" aria-describedby="inputGroup-sizing-sm"/>
+			<div class="input-group-prepend eyeHiddeNo">
+				<span class="input-group-text" id="inputGroup-sizing-sm"><i class="fa fa-eye" aria-hidden="true"></i></span>
+			</div>
+		</div>
 		<small class="text-danger">La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula. NO puede tener otros símbolos.</small>
-	
+	</div>
 	
 	
 	
